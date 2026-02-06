05ad8309ae68419e9c6c997630f14686a2ecf06a
Brian.Yu <38199441+brianYuDesign@users.noreply.github.com>
2026-02-06 09:41:49 +0800

    feat(common): add upload module with Cloudinary setup

---
+++ libs/common/src/upload/upload.module.ts
@@ -0,0 +1,30 @@
+import { Module, DynamicModule, Global } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UploadService } from './upload.service';
+import { configureCloudinary } from './cloudinary.config';
+
+@Global()
+@Module({})
+export class UploadModule {
+  static forRoot(): DynamicModule {
+    return {
+      module: UploadModule,
+      imports: [ConfigModule],
+      providers: [
+        {
+          provide: 'CLOUDINARY_CONFIG',
+          useFactory: (configService: ConfigService) => {
+            return configureCloudinary({
+              cloudName: configService.get<string>('CLOUDINARY_CLOUD_NAME'),
+              apiKey: configService.get<string>('CLOUDINARY_API_KEY'),
+              apiSecret: configService.get<string>('CLOUDINARY_API_SECRET'),
+            });
+          },
+          inject: [ConfigService],
+        },
+        UploadService,
+      ],
+      exports: [UploadService],
+    };
+  }
+}
