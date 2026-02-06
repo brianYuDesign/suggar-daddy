320b98a3da11559bf9be8eee9767fc5892db47b5
Brian.Yu <38199441+brianYuDesign@users.noreply.github.com>
2026-02-06 09:04:16 +0800

    feat(phase2): update payment-service app.module

--- apps/payment-service/src/app/app.module.ts
+++ apps/payment-service/src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TransactionController } from './transaction.controller';
 import { TransactionService } from './transaction.service';
+import { Transaction } from './entities/transaction.entity';
+import { PostPurchase } from './entities/post-purchase.entity';
+import { Tip } from './entities/tip.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forFeature([Transaction, PostPurchase, Tip]),
+  ],
   controllers: [AppController, TransactionController],
   providers: [AppService, TransactionService],
 })
