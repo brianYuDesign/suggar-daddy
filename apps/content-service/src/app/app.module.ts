0020e05acb37f07a197d1699958630d90edaed18
Brian.Yu <38199441+brianYuDesign@users.noreply.github.com>
2026-02-06 09:03:09 +0800

    feat(phase2): update content-service app.module

--- apps/content-service/src/app/app.module.ts
+++ apps/content-service/src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostController } from './post.controller';
 import { PostService } from './post.service';
+import { Post } from './entities/post.entity';
+import { PostLike } from './entities/post-like.entity';
+import { PostComment } from './entities/post-comment.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forFeature([Post, PostLike, PostComment]),
+  ],
   controllers: [AppController, PostController],
   providers: [AppService, PostService],
 })
