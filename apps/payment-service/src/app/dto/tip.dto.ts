e5a2873e9fe9c1b12062bd94cd0fcc4128c4a197
Brian.Yu <38199441+brianYuDesign@users.noreply.github.com>
2026-02-06 09:03:41 +0800

    feat(phase2): add tip DTO

---
+++ apps/payment-service/src/app/dto/tip.dto.ts
@@ -0,0 +1,21 @@
+import { IsString, IsNumber, IsOptional, Min } from 'class-validator';
+
+export class CreateTipDto {
+  @IsString()
+  fromUserId: string;
+
+  @IsString()
+  toUserId: string;
+
+  @IsNumber()
+  @Min(0)
+  amount: number;
+
+  @IsOptional()
+  @IsString()
+  message?: string;
+
+  @IsOptional()
+  @IsString()
+  stripePaymentId?: string;
+}
