463a1607efd23eaf11da818f3c324ca6e03e3e23
Brian.Yu <38199441+brianYuDesign@users.noreply.github.com>
2026-02-12 17:54:15 +0800

    feat(common): export exception handling modules from common library

--- libs/common/src/index.ts
+++ libs/common/src/index.ts
@@ -6,3 +6,9 @@ export * from './stripe';
 export * from './swagger';
 export * from './sharding';
 export * from './filters';
+
+// Exception handling exports
+export * from './lib/error-codes.enum';
+export * from './lib/business-exception';
+export * from './lib/http-exception.filter';
+export * from './lib/exceptions.module';
